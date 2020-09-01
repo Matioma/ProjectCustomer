150b3ad4f25ff84246d71297eb0468b1a0cf619e
Ana-Maria Ilea <62722821+Ana-MariaIlea@users.noreply.github.com>
2020-09-01 13:47:16 +0200

    ClickableObjects

---
+++ Assets/Ana_Scripts/MaterialChange.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialChange : MonoBehaviour
+{
+    [SerializeField]
+    Material defaultMaterial;
+
+    public void ChangeMaterialBack()
+    {
+        var renderer = GetComponent<Renderer>();
+        renderer.material = defaultMaterial;
+    }
+}
