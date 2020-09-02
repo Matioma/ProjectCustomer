79d643ec23723f1214a5249deb4ae1a48093503b
Matioma <Artiom.Matievschi@gmail.com>
2020-09-02 16:06:17 +0200

    JUmping to zones

--- Assets/Scripts/CameraRotation.cs
+++ Assets/Scripts/CameraRotation.cs
@@ -1,23 +1,45 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraRotation : MonoBehaviour
 {
+    static CameraRotation _instance;
+
+    public static CameraRotation Instance {
+        get { return _instance; }
+        set {
+            if (_instance != null) {
+                Debug.LogError("Second Camera Rotation Created, it is being deleted");
+                Destroy(value.gameObject);
+            }
+            else
+            {
+                _instance = value;
+            }
+        }
+    
+    }
+
+
+
     public event Action OnSelectPlanet;
+    public event Action OnDeselectPlanet;
+    public event Action OnSelectZone;
+
+
 
     [SerializeField]
     Planet SelectedPlanet;
+    [SerializeField]
+    Zone SelectedZone;
 
     [SerializeField]
     float RotationSensitivity = 200;
     [SerializeField]
     float ScrollingSpeed = 500;
 
+    bool MousePressed;
 
-
-    bool MousePressed = false;
 
     float MouseXAxis;
     float MouseYAxis;
@@ -26,36 +48,93 @@ public class CameraRotation : MonoBehaviour
 
     private void Awake()
     {
+        Instance = this;
+
         transform.LookAt(SelectedPlanet.transform);
+        
     }
 
-    void LateUpdate()
+    void Update()
     {
         readInput();
         zoomingIn();
         rotateAroundThePlanet();
     }
 
-    void selectPlanet()
-    {
+
+    
+    void selectPlanet(){
         RaycastHit hitResult;
 
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out hitResult, Mathf.Infinity))
         {
-            Planet hitPlanet = hitResult.transform.GetComponent<Planet>();
+            Planet newSelectedPlanet = hitResult.transform.GetComponent<Planet>();
 
-            if (hitPlanet != null && hitPlanet!=SelectedPlanet) {
-                SelectedPlanet = hitPlanet;
+            // If Clicked on a planet and it is different from already Selected Planet
+            if (newSelectedPlanet != null && newSelectedPlanet!=SelectedPlanet) {
+
+                //Deselect the planet if any planet was selected
+                if (SelectedPlanet != null) {
+                    SelectedPlanet.GetComponent<PlanetManager>().Deselect();
+                    OnDeselectPlanet?.Invoke();
+                }
+
+                SelectedPlanet = newSelectedPlanet;
+               
                 transform.LookAt(SelectedPlanet.transform);
+
+                if (SelectedZone != null)
+                {
+                    SelectedZone.GetComponent<ZoneSelection>().IsSelected = false;
+                }
                 zoomAsCloseAsPossible();
+
+                SelectedPlanet.GetComponent<PlanetManager>().Select();
                 OnSelectPlanet?.Invoke();
             }
         }
     }
 
+    void selectZone() {
+        RaycastHit hitResult;
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hitResult, Mathf.Infinity))
+        {
+            Zone newSelectedZone = hitResult.transform.GetComponent<Zone>();
+
+            // If Clicked on a zone and it is different from already Selected Zone
+            if (newSelectedZone != null && newSelectedZone != SelectedPlanet)
+            {
+
+                //Deselect Previous Zone
+                if (SelectedZone != null)
+                {
+                    SelectedZone.GetComponent<ZoneSelection>().IsSelected = false;
+                }
+
+                SelectedZone = newSelectedZone;
+                newSelectedZone.GetComponent<ZoneSelection>().IsSelected = true;
+
+                MoveCameraToZone();
+                OnSelectZone?.Invoke();
+            }
+        }
+    }
 
 
+    void MoveCameraToZone() {
+        if (SelectedPlanet == null || SelectedZone == null) {
+            return;
+        }
+
+        float DistancePlayerToPlanet = (transform.position - SelectedPlanet.transform.position).magnitude;
+
+        Vector3 directionFromPlanetToZone = (SelectedZone.transform.position - SelectedPlanet.transform.position).normalized;
+        transform.position = SelectedPlanet.transform.position + directionFromPlanetToZone * DistancePlayerToPlanet;
+        transform.LookAt(SelectedPlanet.transform);
+    }
 
     void readInput()
     {
@@ -65,16 +144,15 @@ public class CameraRotation : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
-            selectPlanet();
             MousePressed = true;
+            selectPlanet();
+            selectZone();
         }
         if (Input.GetMouseButtonUp(0))
         {
             MousePressed = false;
         }
     }
-
-
 
     void zoomingIn() {
         transform.position += transform.forward * ScrollAxis * ScrollingSpeed;
@@ -100,7 +178,6 @@ public class CameraRotation : MonoBehaviour
             transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0);
         }
     }
-
 
     void zoomAsCloseAsPossible() {
         Vector3 directionTowardsPlayer = (transform.position - SelectedPlanet.transform.position).normalized;
---
+++ Assets/Scripts/ZoneSelection.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneSelection : MonoBehaviour
+{
+    public event Action OnZoneSelected;
+    public event Action OnZoneDeselected;
+
+    bool isSelected;
+    public bool IsSelected {
+        get { return isSelected; }
+        set {
+            if (value == isSelected) {
+                return;
+            }
+            isSelected = value;
+            if (isSelected) { 
+                OnZoneSelected?.Invoke();
+            }
+            else { 
+                OnZoneDeselected?.Invoke();
+            }
+        }
+    }
+
+
+    [SerializeField]
+    Material defaultMaterial;
+    [SerializeField]
+    Material selectedMaterial;
+
+
+    bool MouseOver = false;
+
+    Renderer renderer;
+
+
+    void Awake() {
+        renderer = GetComponent<Renderer>();
+        defaultMaterial = renderer.material;
+
+        OnZoneSelected += () => { renderer.material = selectedMaterial; };
+        OnZoneDeselected += () => { renderer.material = defaultMaterial; };
+    }
+
+    void OnMouseOver() {
+        if (!isSelected)
+        {
+            renderer.material = selectedMaterial;
+        }
+    }
+
+    void OnMouseExit() {
+        if (!isSelected)
+        {
+            renderer.material = defaultMaterial;
+        }
+    }
+}
