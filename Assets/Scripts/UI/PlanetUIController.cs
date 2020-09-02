b1563183a7f2ba4e746b80f1174c34be64d46f19
Matioma <Artiom.Matievschi@gmail.com>
2020-09-02 16:04:01 +0200

    UI Clases

---
+++ Assets/Scripts/UI/PlanetUIController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetUIController : MonoBehaviour
+{
+    [SerializeField]
+    Transform targetCamera;
+
+    private void Start()
+    {
+        targetCamera = CameraRotation.Instance.transform;
+
+        transform.parent.GetComponent<PlanetManager>().OnPlanetSelected += OpenUI;
+    }
+
+    void Update()
+    {
+        transform.LookAt(targetCamera);
+    }
+
+
+
+    void OpenUI() {
+
+    }
+
+
+
+}
