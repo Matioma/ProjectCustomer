1e2eb8f8abc56b20743f22086085c5d89a6f84ae
Matioma <Artiom.Matievschi@gmail.com>
2020-09-02 16:03:27 +0200

    Added PLanet manager
    Keep event that affects this object _ On Selected, On Deselected

---
+++ Assets/Scripts/PlanetManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetManager : MonoBehaviour
+{
+    public event Action OnPlanetSelected;
+    public event Action OnPlanetDeselected;
+
+    bool isSelectedByPlayer =false;
+    bool IsSelectedByPlayer {
+        get{ return isSelectedByPlayer; }
+        set{
+            if (value == isSelectedByPlayer) {
+                return;
+            }
+
+            isSelectedByPlayer = value;
+            if (value) {
+                OnPlanetSelected?.Invoke();
+            }
+            else
+            {
+                OnPlanetDeselected?.Invoke();
+            }
+        }
+    }
+
+    public void Deselect() {
+        IsSelectedByPlayer = false;
+    }
+    public void Select() {
+        IsSelectedByPlayer = true;
+    }
+}
