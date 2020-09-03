9bf2d43a22aba37e362fa35956be1abb90acf686
Ana-Maria Ilea <62722821+Ana-MariaIlea@users.noreply.github.com>
2020-09-04 00:00:14 +0200

    DragDrop
    It still needs to link with the zones

---
+++ Assets/Ana_Scripts/dragDropTest/DragAndDrop.cs
@@ -0,0 +1,50 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler,IEndDragHandler,IDragHandler,IDropHandler  
+{
+    private RectTransform rectTransform;
+    private RectTransform startposition;
+    private Vector2 parentPosition;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        startposition = GetComponentInParent<RectTransform>();
+        parentPosition = new Vector2(startposition.anchoredPosition.x, startposition.anchoredPosition.y);
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("IBeginDragHandler");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("IDragHandler");
+        rectTransform.anchoredPosition += eventData.delta;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("IEndDragHandler");
+       // rectTransform.anchoredPosition = startposition.anchoredPosition;
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerDown");
+
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        //RectTransform delta = new RectTransform();
+        //delta.anchoredPosition = rectTransform.anchoredPosition - startposition.anchoredPosition;
+        //rectTransform.anchoredPosition += delta.anchoredPosition;
+        rectTransform.anchoredPosition = parentPosition;
+        Debug.Log("onDrop" + rectTransform.anchoredPosition);
+    }
+}
--- Assets/Ana_Scripts/goodScripts/PlanetReceources.cs
+++ Assets/Ana_Scripts/goodScripts/PlanetReceources.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlanetReceources : MonoBehaviour, IReceourceAddition<Receources,int>
+public class PlanetReceources : MonoBehaviour, IReceourceAddition<Receources,int>, IEnablable<Receources>
 {
     [SerializeField]
     int moneynumber = 100;
@@ -10,6 +10,16 @@ public class PlanetReceources : MonoBehaviour, IReceourceAddition<Receources,int
     int seednumber = 100;
     [SerializeField]
     int waternumber = 100;
+    [SerializeField]
+    int seedConsumptionAmount = 1;
+    [SerializeField]
+    int seedConsumptionTime = 10;
+    [SerializeField]
+    int waterConsumtionAmount = 1;
+    [SerializeField]
+    int waterConsumtionTime = 10;
+    float waterTimer;
+    float seedTimer;
     Dictionary<Receources, int> receourcesNumber;
 
     void Start()
@@ -18,6 +28,8 @@ public class PlanetReceources : MonoBehaviour, IReceourceAddition<Receources,int
         receourcesNumber.Add(Receources.MONEY, moneynumber);
         receourcesNumber.Add(Receources.SEEDS, seednumber);
         receourcesNumber.Add(Receources.WATER, waternumber);
+        seedTimer = seedConsumptionTime;
+        waterTimer = waterConsumtionAmount;
     }
 
     bool isEnoughReceourse(Receources type)
@@ -30,17 +42,80 @@ public class PlanetReceources : MonoBehaviour, IReceourceAddition<Receources,int
     public void AddReceource(Receources rec, int amount)
     {
         receourcesNumber[rec]+=amount;
+        Debug.Log(rec+"   "+ receourcesNumber[rec]);
+
     }
-   
+
     public int GetReceouceNumber(Receources type)
     {
         return receourcesNumber[type];
     }
     void Update()
     {
-        Debug.Log("seeds: " + receourcesNumber[Receources.SEEDS]);
+        seedConsumption();
+        waterConsumption();
         //Debug.Log("water: " + receourcesNumber[Receources.WATER]);
        // Debug.Log("money: " + receourcesNumber[Receources.MONEY]);
     }
 
+    private void waterConsumption()
+    {
+        if (waterTimer < 0 && receourcesNumber[Receources.WATER] > 0)
+        {
+            if (receourcesNumber[Receources.WATER] > waterConsumtionAmount)
+            {
+                AddReceource(Receources.WATER, -waterConsumtionAmount);
+            }
+            else
+            {
+                AddReceource(Receources.WATER, -receourcesNumber[Receources.WATER]);
+            }
+            waterTimer = waterConsumtionTime;
+        }
+        else
+        {
+            waterTimer -= Time.fixedDeltaTime;
+        }
+    }
+
+    private void seedConsumption()
+    {
+        if (seedTimer < 0 && receourcesNumber[Receources.SEEDS] > 0)
+        {
+            if (receourcesNumber[Receources.SEEDS] > seedConsumptionAmount)
+            {
+                AddReceource(Receources.SEEDS, -seedConsumptionAmount);
+            }
+            else
+            {
+                AddReceource(Receources.SEEDS, -receourcesNumber[Receources.SEEDS]);
+            }
+            seedTimer = seedConsumptionTime;
+        }
+        else
+        {
+            seedTimer -= Time.fixedDeltaTime;
+        }
+    }
+
+    void conditionsForZones()
+    {
+
+    }
+
+    public void Enable(Receources rec)
+    {
+        if (rec == Receources.ALL)
+        {
+            this.enabled = true;
+        }
+    }
+
+    public void Disable(Receources rec)
+    {
+        if (rec == Receources.ALL)
+        {
+            this.enabled = false;
+        }
+    }
 }
