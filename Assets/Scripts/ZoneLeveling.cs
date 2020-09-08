89c007a2f39e6bfe8c72b97f69aabb4a91dab95c
Matioma <Artiom.Matievschi@gmail.com>
2020-09-08 11:22:13 +0200

    Zone builder

---
+++ Assets/Scripts/ZoneLeveling.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneLeveling : MonoBehaviour
+{
+    [SerializeField]
+    int CurrentLevel=0;
+
+    public List<GameObject> LevelsPrefabs;
+
+    private void Start()
+    {
+        RemoveOldPrefabs();
+        ChangePrefab();
+    }
+
+    public void Upgrade()
+    {
+        if (CurrentLevel < LevelsPrefabs.Count)
+        {
+            CurrentLevel++;
+            RemoveOldPrefabs();
+            ChangePrefab();
+        }
+        else{
+            Debug.Log("Tried to Upgrade region out of it max capacity");
+        }
+    }
+
+    void ChangePrefab() {
+        LevelsPrefabs[CurrentLevel].SetActive(true);
+    }
+
+    void RemoveOldPrefabs() {
+        foreach (Transform child in this.transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+    }
+}
