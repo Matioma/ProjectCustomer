4b3faff194c222bfc93c577af83e45675df1996c
Matioma <Artiom.Matievschi@gmail.com>
2020-09-08 15:48:11 +0200

    New Prefabs For rest of planets

---
+++ Assets/Scripts/SpaceShip/SpaceShipController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShipController : MonoBehaviour
+{
+    [SerializeField]
+    Planet targetPlanet;
+
+    [SerializeField]
+    float speed;
+    public void SetTargetPlanet(Planet ptargetPlanet)
+    {
+        targetPlanet = ptargetPlanet;
+    }
+    void Update()
+    {
+        if (targetPlanet == null) {
+            Debug.Log("SpaceShip has no targetPlanet");
+            return;
+        }
+
+        transform.LookAt(targetPlanet.transform);
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/SpaceShip/SpaceShipManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShipManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject SpaceShipPrefab;
+
+
+    [SerializeField]
+    Planet sendingPlanet;
+
+    [SerializeField]
+    Planet targetPlanet;
+
+    public void SendShip(Planet from, Planet to) {
+        GameObject spaceShipRef =Instantiate(SpaceShipPrefab, from.transform.position, Quaternion.identity);
+        spaceShipRef.GetComponent<SpaceShipController>()?.SetTargetPlanet(to);
+    }
+    void Start()
+    {
+        if (sendingPlanet != null && targetPlanet != null) {
+            SendShip(sendingPlanet, targetPlanet);
+        }
+    }
+}
