a38ff77d127cb5cc551ee1f29c079a07236d5a48
Matioma <Artiom.Matievschi@gmail.com>
2020-09-09 09:17:26 +0200

    SpaceShip  Collision detection

--- Assets/Scripts/SpaceShip/SpaceShipManager.cs
+++ Assets/Scripts/SpaceShip/SpaceShipManager.cs
@@ -1,9 +1,39 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class SpaceShipManager : MonoBehaviour
 {
+    SpaceShipManager _instance;
+
+    public SpaceShipManager Instance
+    {
+        get {
+            if (_instance == null) {
+                Debug.Log("There is No SpaceShipManager in the level");
+            }
+            return _instance; 
+        }
+        set{
+            if (_instance == null)
+            {
+                _instance = value;
+            }
+            else
+            {
+                Debug.Log("Tryingto create second SpaceShipManager, destorying it");
+                Destroy(value);
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
     [SerializeField]
     GameObject SpaceShipPrefab;
 
