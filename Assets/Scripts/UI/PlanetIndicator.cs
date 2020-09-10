91c19e5223a75bf00f5838bf3d23951c86b906c0
Matioma <Artiom.Matievschi@gmail.com>
2020-09-10 11:16:36 +0200

    Planet Indicator

---
+++ Assets/Scripts/UI/PlanetIndicator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetIndicator : MonoBehaviour
+{
+    Camera camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(camera.transform);
+    }
+}
