55b64d0cafff4c20e4ea3f4c492dc630ac6094ac
Matioma <Artiom.Matievschi@gmail.com>
2020-09-11 16:01:00 +0200

    Add PlanetUI prefab

---
+++ Assets/Scripts/UI/ProductionFeedbackManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProductionFeedbackManager : MonoBehaviour
+{
+    //List<ReceourceZone> planetReceourceZones;
+    
+    void Start()
+    {
+        var planetReceurcesZones = GetComponentInParent<Planet>().GetComponentsInChildren<ReceourceZone>();
+        foreach (ReceourceZone receourceZone in planetReceurcesZones) {
+            receourceZone.onEndProductionCycle += ()=> {
+                Debug.Log("One Cycle done " + receourceZone.GetProductionAmount() + " " + receourceZone.GetResourceType());
+            };
+        }
+    }
+
+    void DisplayMessage()
+    {
+
+        Debug.Log("One Cycle done" );
+    }
+    void Update()
+    {
+        
+    }
+}
